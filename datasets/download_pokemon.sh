c8b8d28764df3f0305542c034c855bf8788f3060
Miguel Samaniego <ma.samaniego@hotmail.com>
2025-05-22 17:04:04 +0000

    Call python script from bash script

--- datasets/download_pokemon.sh
+++ datasets/download_pokemon.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -e
 echo "Downloading Pokémon dataset from Kaggle..."
 curl -L -o ./pokemon-images-and-types.zip\
@@ -6,3 +5,4 @@ curl -L -o ./pokemon-images-and-types.zip\
 unzip -o ./pokemon-images-and-types.zip -d ./datasets/pokemon
 rm ./pokemon-images-and-types.zip
 echo "Pokémon dataset downloaded and extracted to ./datasets/pokemon ✅"
+python datasets/prepare_pokemon_dataset.py
